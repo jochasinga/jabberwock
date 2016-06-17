ed5ac5b5d0394c8284a4f30c2a7c5a727e7bb4b5
Joe Chasinga <jo.chasinga@gmail.com>
2016-06-16 21:04:30 -0400

    Back to basic

--- crawler.py
+++ crawler.py
@@ -3,54 +3,31 @@ from __future__ import print_function
 import os
 import re
 import logging
-import requests
-import responses
+
 # Python 3 is urllib.parse
 from urlparse import urlsplit
 from bs4 import BeautifulSoup
+
 import gevent
 from gevent import Greenlet
 from gevent.queue import Queue, Empty
-from gevent.pool import Pool
+from gevent.pool import Group
+from gevent import monkey
+monkey.patch_all()
+
+import requests
+import responses
 
 logger = logging.getLogger('general')
 
 class CrawlBase(object):
-    """
-    CrawlBase is a singleton Pool which acts like a head quarter spawning crawlers.
-    Only one is needed in running a crawl operation.
-    """
-    silo = Queue()
     spider_count = 0
 
-    def __init__(self, max_pool=100):
-        """The contructor create a pool  with the default of
-           max_pool = 100 and start it.
-           TODO: Write tests
-        """
-        self.pool = Pool(max_pool)
-        #self.pool.start()
+    def __init__(self):
+        self.group = Group()
+        self.queue = Queue()
 
     def read_seed(self, file='seeds.txt'):
-        """By default, CrawlBase reads seed URLs from a text file
-
-        >>> urls = c.read_seed('seeds.txt')
-        >>> urls.next()
-        'http://digg.com'
-        >>> urls.next()
-        'http://www.buzzfeed.com'
-        >>> urls.next()
-        'http://www.bustle.com'
-        >>> urls.next()
-        'https://en.wikipedia.org'
-        >>> urls.next()
-        'https://news.ycombinator.com'
-        >>> urls.next()
-        Traceback (most recent call last):
-        ...
-        StopIteration
-
-        """
         with open(file) as f:
             for line in f:
                 if len(line) > 0 and line != "\n":
@@ -59,68 +36,43 @@ class CrawlBase(object):
                     return
 
     def dispatch(self):
-        """Spawn more spider"""
-        if self.pool.full():
-            raise Exception("Maximum pool size reached")
-        else:
-            self.pool.map(Spider, self.read_seed())
+        for url in self.read_seed():
+            g = gevent.spawn(Spider, self, url)
+            self.group.add(g)
+
+        self.group.join()
 
     def harvest(self):
-        """Harvest from the silo"""
         try:
             while True:
-                content = self.silo.get(timeout=2)
-                # test printing only
+                content = self.queue.get(timeout=2)
                 print(content)
-                gevent.sleep(0)
         except Empty:
-            logger.info("Silo is empty.")
-            self.kill()
+            pass
 
-    def kill(self):
-        self.pool.kill()
-
-class Spider(Greenlet):
+#class Spider(Greenlet):
+class Spider(object):
     """Crawler is a worker instance which fetch and crawl the web
        and put the soupified content into the CrawlBase's silo.
     """
-    #name, email = None
 
-    def __init__(self, url=None, name='Charlotte', depth=1, **kwargs):
-        """The contructor has a default name 'Charlotte' and depth = 1.
+    name = 'Charlotte'
+    email = None
 
-        >>> s.name
-        'Charlotte'
-        >>> s.storage != None
-        True
-        >>> s.depth
-        1
-        >>> isinstance(sp, Greenlet)
-        True
-        >>> sp.name
-        'Charlotte'
-        >>> sp.depth
-        3
-        >>> sp.org
-        'Looqsie'
-        >>> sp.website
-        'looqsie.com'
-        >>> sp.email
-        'jo@looqsie.com'
-        >>> sp.message
-        'Hello! Thank you.'
+    def __init__(self, base, url=None, name=name, depth=1, **kwargs):
+        """The contructor has a default name 'Charlotte' and depth = 1"""
 
-        """
-        self.id      = CrawlBase.spider_count
-        self.name    = name
-        self.depth   = depth
-        self.org     = None
-        self.website = None
-        self.email   = None
-        self.message = None
-        self.rules   = None
-        self.headers = None
-        self.storage = Queue()
+        self.base     = base
+        self._id      = self.base.spider_count
+        self.name     = name
+        self.seed_url = url
+        self.depth    = depth
+        self.org      = None
+        self.website  = None
+        self.email    = None
+        self.message  = None
+        self.rules    = None
+        self.headers  = None
 
         for key, arg in kwargs.iteritems():
             if key == 'org':
@@ -136,25 +88,15 @@ class Spider(Greenlet):
 
         self.prepare_headers()
 
-        CrawlBase.spider_count += 1
+        base.spider_count += 1
 
-        Greenlet.__init__(self)
+        #Greenlet.__init__(self)
+
+        self.prepare_headers()
+        self.run()
 
     def prepare_headers(self):
-        """Prepare the appropriate headers to send with the requests
-
-        >>> sp.headers['user-agent']['name']
-        'Charlotte'
-        >>> sp.headers['user-agent']['org']
-        'Looqsie'
-        >>> sp.headers['user-agent']['website']
-        'looqsie.com'
-        >>> sp.headers['user-agent']['email']
-        'jo@looqsie.com'
-        >>> sp.headers['user-agent']['message']
-        'Hello! Thank you.'
-
-        """
+        """Prepare the appropriate headers to send with the requests"""
 
         if self.headers is None:
             self.headers = {'user-agent': {'name': self.name}}
@@ -168,186 +110,39 @@ class Spider(Greenlet):
         if self.message is not None:
             self.headers['user-agent']['message'] = self.message
 
-    def fetch_rules(self, url):
-        """
-        Fetch the host's robots.txt, break it up into an agent dictionary in this
-        structure => agent_dict[u'User-agent'] = {u'Allow': [url1, url2, ...]}
-        for read_rules() to match to self.name
-
-        >>> sp.headers is not None
-        True
-        >>> sp.headers['user-agent']['name']
-        'Charlotte'
-        >>> agent_dict = sp.fetch_rules('https://en.wikipedia.org')
-        >>> agent_dict is not None
-        True
-        >>> len(agent_dict) > 0
-        True
-        >>>
-        """
-        agent_dict = None
-        scheme = urlsplit(url).scheme
-        hostname = urlsplit(url).hostname
-        addr = scheme + '://' + hostname
-        robots_dst = os.path.join(addr, 'robots.txt')
-
-        try:
-            resp = requests.get(robots_dst, headers=self.headers, timeout=2)
-        except requests.exceptions.RequestException as e:
-            logger.error(e)
-        else:
-            pass
-
-        # Presume no robots.txt
-        if resp.status_code != 200:
-            logger.debug("No robots.txt found.")
-            return agent_dict
-        else:
-            agent_dict = {}
-            lines = resp.text.splitlines()
-            for l in lines:
-                pair = map(lambda s: s.strip(), l.split(':', 1))
-                if len(pair) == 2:
-                    if pair[0] == 'User-agent'and pair[1] not in agent_dict:
-                        key = pair[1]
-                        agent_dict[key] = {}
-                    else:
-                        for agent in agent_dict:
-                            if pair[0] not in agent_dict[agent]:
-                                agent_dict[agent][pair[0]] = []
-                                agent_dict[agent][pair[0]].append(pair[1])
-                            else:
-                                agent_dict[agent][pair[0]].append(pair[1])
-
-        return agent_dict
-
-    def read_rules(self, agent_dict):
-        """
-        Take an agent dictionary, investigate the user-agent key,
-        and if the key matches the name of this spider's user-agent,
-        set self.rules to the dictionary of that matched user-agent.
-
-        >>> for_all = {u'*': {u'Disallow': [u'/', u'/forbidden']}}
-        >>> s.read_rules(for_all)
-        True
-        >>> s.rules
-        {u'Disallow': [u'/', u'/forbidden']}
-        >>> for_charlotte = {u'Charlotte': {u'Allow': [u'/goahead']}}
-        >>> s.read_rules(for_charlotte)
-        True
-        >>> s.rules
-        {u'Allow': [u'/goahead']}
-        >>> for_others = {u'Anonymous Spider': {u'Allow': [u'/spiderhere']}}
-        >>> s.read_rules(for_others)
-        False
-        >>> print(s.rules)
-        None
-        >>> no_rule = None
-        >>> s.read_rules(no_rule)
-        False
-        >>> print(s.rules)
-        None
-
-        """
-        if agent_dict is not None:
-            for agent in agent_dict:
-                if agent == '*' or agent == self.name:
-                    self.rules = agent_dict[agent]
-                    return True
-                else:
-                    logger.debug("No rules matched for this spider's name.")
-                    self.rules = None
-                    return False
-        else:
-            logger.debug("agent_dict is None")
-            self.rules = None
-            return False
-
-    def fetch(self, url):
-        try:
-            response = requests.get(url, headers=self.headers, timeout=2)
-        except requests.exceptions.RequestException as e:
-            logger.error(e)
-            return None
-        else:
-            return response.content
-
-    def get_url_route(self, url):
-        """Return the full route of a URL (path + query + fragment)
-
-        >>> sp.get_url_route('http://dummy.com/path1/path2?q=something#frag')
-        '/path1/path2?q=something#frag'
-
-        """
-        p = urlsplit(url)
-        path = ''
-        query = ''
-        fragment = ''
-
-        if len(p.path) > 0:
-            path = p.path
-        if len(p.query) > 0:
-            query = '?' + p.query
-        if len(p.fragment) > 0:
-            fragment = '#' + p.fragment
-
-        route = path + query + fragment
-        return route
-
     def crawl(self, url, level=1):
-        """Fetch content from URL and put soup on queue
-
-        >>> s.crawl("https://github.com", 1)
-        >>> soup = CrawlerBase.silo.get()
-        >>> soup is not None
-        True
-        >>> isinstance(soup, BeautifulSoup)
-        True
-
-        """
-
-        while level >= 0:
-            agent_dict = self.fetch_rules(url)
-            self.read_rules(agent_dict)
-            if self.rules is not None:
-                if self.strict:
-                    if self.get_url_route(url) not in self.rules['Allow']:
-                        return
-                elif not self.strict:
-                    if self.get_url_route(url) not in self.rules['Disallow']:
-                        # start fetching content
-                        html = self.fetch(url)
-                        soup = BeautifulSoup(html, 'lxml')
-                        if soup is not None:
-                            # save the first-level soup to base
-                            self.stash(soup)
-                            # and go on recursing into each link
-                            if len(soup.find_all('a')) > 0:
-                                for a in soup.find_all('a'):
-                                    if len(a['href']) > 0:
-                                        level -= 1
-                                        self.crawl(a['href'], level)
-                    else:
-                        return
-                else:
-                    raise TypeError('Wrong type for self.strict (must be bool).')
+        while level > 0:
+            try:
+                resp = requests.get(url, timeout=4, headers=self.headers)
+            except requests.exceptions.RequestException as e:
+                print(e.message)
+                continue
+            else:
+                if resp is not None and resp.status_code == 200:
+                    level -= 1
+                    soup = BeautifulSoup(resp.content, "lxml")
+                    self.stash(soup)
 
     def stash(self, soup):
-        CrawlBase.silo.put(soup)
-        gevent.sleep(0)
+        self.base.queue.put(soup)
 
-    def _run(self):
-        self.running = True
+    def run(self):
+        #self.running = True
 
-        while self.running:
-            self.crawl(CrawlBase.seed_urls, self.depth)
+        while True:
+            self.crawl(self.seed_url, self.depth)
 
 if __name__ == '__main__':
+    """
     import doctest
     doctest.testmod(extraglobs={
         'c': CrawlBase(),
-        's': Spider(url='http://digg.com'),
+        's': Spider(url='http://digg.com', base=c),
         'sp': Spider(url='http://digg.com', depth=3, org='Looqsie',
                      website='looqsie.com', email='jo@looqsie.com',
                      message='Hello! Thank you.')
     })
+    """
+    cb = CrawlBase()
+    cb.dispatch()
+    cb.harvest()
